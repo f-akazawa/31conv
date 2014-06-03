773c948d9ebee60200ec51daf034db261cd36a90
Fumihiko.akazawa <akazawa@jamstec.go.jp>
2014-06-03 11:31:08 +0900

    データベースからの読み書き以外はとりあえず完成版
    ファイルの上書きチェックと既存の変数の変更が出来ないので
    、出来上がりのファイル（updatefile.nc)を削除しないと複数回のテストが出来ない

--- tooltest.m
+++ tooltest.m
@@ -1,7 +1,68 @@
-copyfile(which('Rtest.nc'),'c:\Github\31conv\myfile.nc');
-%fileattrib('myfile.nc','+w');
+path = 'c:\Github\31conv\';
+updatefile = 'updatefile.nc';
+readfile = 'myfile.nc';
+origfile = 'Rtest.nc';
+
+% オリジナルを消さないように作業ファイルにコピーする
+copyfile(which([path origfile]),[path readfile]);
+
+finfo = ncinfo([path readfile]);
+
+% netcdf 読み込み
+% dimensions
+for i1=1:size(finfo.Dimensions,2)
+    eval([finfo.Dimensions(i1).Name '=finfo.Dimensions(i1).Length;']);
+end
+
+% valiables
+for i1=1:size(finfo.Variables,2)
+    eval([finfo.Variables(i1).Name '=ncread([readfile],finfo.Variables(i1).Name);']);
+end
+
+% 読み込んだので書きだす
+% INST_REFERENCEは削除された変数なので、そこだけは書かないようにして書きだす
+
+ncid1 = netcdf.create([path updatefile],'NC_NOCLOBBER');
+for i1=1:size(finfo.Dimensions,2)
+    if strcmp(finfo.Dimensions(i1).Name,'N_HISTORY') == 0
+        netcdf.defDim(ncid1,finfo.Dimensions(i1).Name,finfo.Dimensions(i1).Length);
+    end
+end
+netcdf.defDim(ncid1,'N_HISTORY',netcdf.getConstant('NC_UNLIMITED'));
+netcdf.close(ncid1);
+
+
+% Variables INST_REFERENCEだったらそこは書かない。
+% write netcdf contents
+for i1=1:size(finfo.Variables,2)
+
+    % variables
+    ex1='';
+    for i2=1:size(finfo.Variables(i1).Dimensions,2)
+        ex1=[ex1 '''' finfo.Variables(i1).Dimensions(i2).Name ''',' num2str(finfo.Variables(i1).Dimensions(i2).Length) ','];
+    end
+    ex1=ex1(1:end-1);
+    
+    if strcmp(finfo.Variables(i1).Name,'INST_REFERENCE') == 0
+          eval(['nccreate(updatefile,finfo.Variables(i1).Name,''Dimensions'',{' ex1 '},''Datatype'',finfo.Variables(i1).Datatype,''Format'',''classic'');'])
+
+        % attributes
+        for i3=1:size(finfo.Variables(i1).Attributes,2)
+            hoge = finfo.Variables(i1).Name;
+            ncwriteatt(updatefile,finfo.Variables(i1).Name,finfo.Variables(i1).Attributes(i3).Name,finfo.Variables(i1).Attributes(i3).Value);
+        end
+    
+
+        % data
+        eval(['ncwrite(updatefile,finfo.Variables(i1).Name,' finfo.Variables(i1).Name ');'])
+    end
+end
+
+
+
+
 % file open
-ncid = netcdf.open('myfile.nc','NC_WRITE')
+ncid = netcdf.open('updatefile.nc','NC_WRITE')
 
 % 定義モードにする
 netcdf.reDef(ncid);
@@ -63,117 +124,63 @@ netcdf.sync(ncid);
 netcdf.close(ncid);
 
 % 3.1で増えた変数を追加
-nccreate('myfile.nc','PLATFORM_TYPE',...
+nccreate('updatefile.nc','PLATFORM_TYPE',...
     'Dimensions',{'N_PROF','STRING32'},...
     'Datatype','char');
-ncwriteatt('myfile.nc','PLATFORM_TYPE','long_name','Type of float');
-ncwriteatt('myfile.nc','PLATFORM_TYPE','_FillValue',' ');
+ncwriteatt('updatefile.nc','PLATFORM_TYPE','long_name','Type of float');
+ncwriteatt('updatefile.nc','PLATFORM_TYPE','conventions','Argo reference table 23');
+ncwriteatt('updatefile.nc','PLATFORM_TYPE','_FillValue',' ');
 
-nccreate('myfile.nc','FLOAT_SERIAL_NO',...
+nccreate('updatefile.nc','FLOAT_SERIAL_NO',...
     'Dimensions',{'N_PROF','STRING32'},...
     'Datatype','char');
-ncwriteatt('myfile.nc','FLOAT_SERIAL_NO','long_name','Serial number of the float');
-ncwriteatt('myfile.nc','FLOAT_SERIAL_NO','_FillValue',' ');
+ncwriteatt('updatefile.nc','FLOAT_SERIAL_NO','long_name','Serial number of the float');
+ncwriteatt('updatefile.nc','FLOAT_SERIAL_NO','_FillValue',' ');
 
-nccreate('myfile.nc','FIRMWARE_VERSION',...
+nccreate('updatefile.nc','FIRMWARE_VERSION',...
     'Dimensions',{'N_PROF','STRING16'},...
     'Datatype','char');
-ncwriteatt('myfile.nc','FIRMWARE_VERSION','long_name','Instrument firmware version');
-ncwriteatt('myfile.nc','FIRMWARE_VERSION','_FillValue',' ');
+ncwriteatt('updatefile.nc','FIRMWARE_VERSION','long_name','Instrument firmware version');
+ncwriteatt('updatefile.nc','FIRMWARE_VERSION','_FillValue',' ');
 
-nccreate('myfile.nc','VERTICAL_SAMPLING_SCHEME',...
+nccreate('updatefile.nc','VERTICAL_SAMPLING_SCHEME',...
     'Dimensions',{'N_PROF','STRING256'},...
     'Datatype','char');
-ncwriteatt('myfile.nc','VERTICAL_SAMPLING_SCHEME','long_name','Vertical sampling scheme');
-ncwriteatt('myfile.nc','VERTICAL_SAMPLING_SCHEME','conventions','Argo reference table 16');
-ncwriteatt('myfile.nc','VERTICAL_SAMPLING_SCHEME','_FillValue',' ');
+ncwriteatt('updatefile.nc','VERTICAL_SAMPLING_SCHEME','long_name','Vertical sampling scheme');
+ncwriteatt('updatefile.nc','VERTICAL_SAMPLING_SCHEME','conventions','Argo reference table 16');
+ncwriteatt('updatefile.nc','VERTICAL_SAMPLING_SCHEME','_FillValue',' ');
 
-nccreate('myfile.nc','CONFIG_MISSION_NUMBER',...
+nccreate('updatefile.nc','CONFIG_MISSION_NUMBER',...
     'Dimensions',{'N_PROF'},'Datatype','int32');
-ncwriteatt('myfile.nc','CONFIG_MISSION_NUMBER','long_name','Float mission number of each profile');
-ncwriteatt('myfile.nc','CONFIG_MISSION_NUMBER','conventions','1..N , 1:first complete mission');
-ncwriteatt('myfile.nc','CONFIG_MISSION_NUMBER','_FillValue',99999);
+ncwriteatt('updatefile.nc','CONFIG_MISSION_NUMBER','long_name','Float mission number of each profile');
+ncwriteatt('updatefile.nc','CONFIG_MISSION_NUMBER','conventions','1..N , 1:first complete mission');
+ncwriteatt('updatefile.nc','CONFIG_MISSION_NUMBER','_FillValue',99999);
 
 % 3.1で追加になったアトリビュートを追加
-ncwriteatt('myfile.nc','JULD','standard_name','time');
-ncwriteatt('myfile.nc','JULD','resolution','X');
-ncwriteatt('myfile.nc','JULD','axis','T');
+ncwriteatt('updatefile.nc','JULD','standard_name','time');
+ncwriteatt('updatefile.nc','JULD','resolution','X');
+ncwriteatt('updatefile.nc','JULD','axis','T');
 
-ncwriteatt('myfile.nc','JULD_LOCATION','resolution','X');
+ncwriteatt('updatefile.nc','JULD_LOCATION','resolution','X');
 
-ncwriteatt('myfile.nc','LATITUDE','standard_name','latitude');
-ncwriteatt('myfile.nc','LATITUDE','axis','Y');
+ncwriteatt('updatefile.nc','LATITUDE','standard_name','latitude');
+ncwriteatt('updatefile.nc','LATITUDE','axis','Y');
 
-ncwriteatt('myfile.nc','LONGITUDE','standard_name','longitude');
-ncwriteatt('myfile.nc','LONGITUDE','axis','X');
+ncwriteatt('updatefile.nc','LONGITUDE','standard_name','longitude');
+ncwriteatt('updatefile.nc','LONGITUDE','axis','X');
 
-ncwriteatt('myfile.nc','SCIENTIFIC_CALIB_DATE','long_name','Date of calibration');
+ncwriteatt('updatefile.nc','SCIENTIFIC_CALIB_DATE','long_name','Date of calibration');
 
-ncwriteatt('myfile.nc','PRES','standard_name','PRES');
-ncwriteatt('myfile.nc','TEMP','standard_name','TEMP');
-ncwriteatt('myfile.nc','PSAL','standard_name','PSAL');
+ncwriteatt('updatefile.nc','PRES','standard_name','PRES');
+ncwriteatt('updatefile.nc','TEMP','standard_name','TEMP');
+ncwriteatt('updatefile.nc','PSAL','standard_name','PSAL');
 
-ncwriteatt('myfile.nc','PRES_ADJUSTED','standard_name','PRES_ADJUSTED');
-ncwriteatt('myfile.nc','TEMP_ADJUSTED','standard_name','TEMP_ADJUSTED');
-ncwriteatt('myfile.nc','PSAL_ADJUSTED','standard_name','PSAL_ADJUSTED');
-
-
-% INST_REFERENCEは削除された変数なので、そこだけは書かないようにして最終的に書きだす
-path = 'c:\Github\31conv\';
-updatefile = 'updatefile.nc';
-readfile = 'myfile.nc';
-
-finfo = ncinfo([readfile]);
-
-% netcdf 読み込み
-% dimensions
-for i1=1:size(finfo.Dimensions,2)
-    eval([finfo.Dimensions(i1).Name '=finfo.Dimensions(i1).Length;']);
-end
-
-% valiables
-for i1=1:size(finfo.Variables,2)
-    eval([finfo.Variables(i1).Name '=ncread([readfile],finfo.Variables(i1).Name);']);
-end
-
-% 読み込んだので書きだす
-% dimensionsについてはよくわからんので中島さんソースのまま。
-% ncid = netcdf.create([path updatefile],'NC_NOCLOBBER');
-
-% テスト中は存在チェックしない
-ncid = netcdf.create([path updatefile],'NC_CLOBBER');
-for i1=1:size(finfo.Dimensions,2)
-    if strcmp(finfo.Dimensions(i1).Name,'N_HISTORY') == 0
-        netcdf.defDim(ncid,finfo.Dimensions(i1).Name,finfo.Dimensions(i1).Length);
-    end
-end
-netcdf.defDim(ncid,'N_HISTORY',netcdf.getConstant('NC_UNLIMITED'));
-netcdf.close(ncid);
+ncwriteatt('updatefile.nc','PRES_ADJUSTED','standard_name','PRES_ADJUSTED');
+ncwriteatt('updatefile.nc','TEMP_ADJUSTED','standard_name','TEMP_ADJUSTED');
+ncwriteatt('updatefile.nc','PSAL_ADJUSTED','standard_name','PSAL_ADJUSTED');
 
 
-% Variables INST_REFERENCEだったらそこは書かない。
-% write netcdf contents
-for i1=1:size(finfo.Variables,2)
 
-    % variables
-    ex1='';
-    for i2=1:size(finfo.Variables(i1).Dimensions,2)
-        ex1=[ex1 '''' finfo.Variables(i1).Dimensions(i2).Name ''',' num2str(finfo.Variables(i1).Dimensions(i2).Length) ','];
-    end
-    ex1=ex1(1:end-1);
-    
-    if strcmp(finfo.Variables(i1).Name,'INST_REFERENCE') == 0
-      eval(['nccreate(updatefile,finfo.Variables(i1).Name,''Dimensions'',{' ex1 '},''Datatype'',finfo.Variables(i1).Datatype,''Format'',''classic'');'])
-    end
-
-    % attributes
-    for i3=1:size(finfo.Variables(i1).Attributes,2)
-        ncwriteatt(updatefile,finfo.Variables(i1).Name,finfo.Variables(i1).Attributes(i3).Name,finfo.Variables(i1).Attributes(i3).Value);
-    end
-
-    % data
-    eval(['ncwrite(updatefile,finfo.Variables(i1).Name,' finfo.Variables(i1).Name ');'])
-end
 
 
 
