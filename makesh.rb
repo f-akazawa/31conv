66d28888da9511ca851f06ef250aaa31a877d6ef
akazawa <akazawa@akazawa02.hq.jamstec.go.jp>
2014-08-26 14:51:36 +0900

    配列には改行入り、配列の要素は式展開しないとマッチに使えない

--- makesh.rb
+++ makesh.rb
@@ -1,4 +1,5 @@
 ﻿#!/usr/local/bin/ruby
+# -*- coding: utf-8-with-signature -*-
 
 ## ARGOS通信のあるWMO番号リストをDBから作ってもらったので
 ## 自作のプロファイル3.1変換スクリプトを実行するファイルのリストを作成するためのツール
@@ -24,26 +25,30 @@ fulllist = File.open("convall.sh","r")
 # 配列に保存
 full_array = Array.new
 
-	fulllist.each_line{|line|
-		full_array.push line
+	fulllist.each_line{|line2|
+		full_array.push line2
 	}
 fulllist.close
 
 # マッチした行を確認して新たなファイルに書き出す
 out = File.open("out.sh","w")
-hoge = 0
-for match_wmo in wmo_list_array do
-	for var in full_array do
-		# match_wmo : ARGOS通信のフロートWMO番号
-		# var : 全Dファイルのフルパス、matlabコマンド実行スクリプト
-		# 両者を比較してマッチしたvarを新しいファイルに書き出す。
-		if var.to_s.include?(match_wmo.to_s) then
-		# マッチしない・・・なぜ？
-			#out.write(var.to_s)
-			hoge = hoge +1
-		end
+for wmo_no in wmo_list_array do
+reg = Regexp.new("#{wmo_no}".chomp!)
+# p wmo_no
+  	for var in full_array do
+          str2 = "#{var}"
+          # match_wmo : ARGOS通信のフロートWMO番号
+          # var : 全Dファイルのフルパス、matlabコマンド実行スクリプト
+          # 両者を比較してマッチしたvarを新しいファイルに書き出す。
+          # 配列には改行付きで入っていたので削る必要あり
+          # るーぷの要素も式展開をしないとぱたーんまっちでは使えない
+          # 以上の点に非常にはまってしまった。
+          if reg =~ str2 then
+            # マッチしたら書き出し
+            out.write(str2)
+            else
+          end
 	end
-	puts hoge 
 end
 out.close
 
