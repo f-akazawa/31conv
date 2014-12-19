c2e6bd15cd20fc5abdc266651945301ef63f5f44
Fumihiko Akazawa <akazawa@jamstec.go.jp>
2014-12-19 13:22:13 +0900

    PSALの存在チェック部分をテストしているコード

--- conv31.m
+++ conv31.m
@@ -120,7 +120,6 @@ delFuncID = netcdf.inqVarID(ncid,'PRES');
 netcdf.delAtt(ncid,delFuncID,'comment');
 delFuncID2 = netcdf.inqVarID(ncid,'PRES_ADJUSTED');
 netcdf.delAtt(ncid,delFuncID2,'comment');
-delFuncID3 = netcdf.inqVarID(ncid,'PRES_ADJUSTED_ERROR');
 netcdf.delAtt(ncid,delFuncID3,'comment');
 
 delFuncID4 = netcdf.inqVarID(ncid,'TEMP');
@@ -130,12 +129,17 @@ netcdf.delAtt(ncid,delFuncID5,'comment');
 delFuncID6 = netcdf.inqVarID(ncid,'TEMP_ADJUSTED_ERROR');
 netcdf.delAtt(ncid,delFuncID6,'comment');
 
-delFuncID7 = netcdf.inqVarID(ncid,'PSAL');
-netcdf.delAtt(ncid,delFuncID7,'comment');
-delFuncID8 = netcdf.inqVarID(ncid,'PSAL_ADJUSTED');
-netcdf.delAtt(ncid,delFuncID8,'comment');
-delFuncID9 = netcdf.inqVarID(ncid,'PSAL_ADJUSTED_ERROR');
-netcdf.delAtt(ncid,delFuncID9,'comment');
+% プロファイルによってはPSALが無いものがあるので以下は存在チェック
+%if(netcdf.inqVarID(ndid,'PSAL') != NaN)
+    delFuncID7 = netcdf.inqVarID(ncid,'PSAL');
+    netcdf.delAtt(ncid,delFuncID7,'comment');
+    delFuncID8 = netcdf.inqVarID(ncid,'PSAL_ADJUSTED');
+    netcdf.delAtt(ncid,delFuncID8,'comment');
+    delFuncID9 = netcdf.inqVarID(ncid,'PSAL_ADJUSTED_ERROR');
+    netcdf.delAtt(ncid,delFuncID9,'comment');
+%else
+    
+%end
 
 % SCIENTIFIC_CALIB_DATEはCALIBLATION_DATEをリネームして利用している
 % FillValueを下に追加したいので一度項目を削除してあとで順番に追加する
